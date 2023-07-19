4a0f72f46c4fcfe60187f4f62d69d11ddf9da0d6
JANGHYUNGSEOK\장형석 <hsjang04@gmail.com>
2023-07-19 17:49:00 +0900

    Implement Lerp motion in character pick UI and change hierachy of selectbar

--- Assets/Scripts/UI/PickCharacter.cs
+++ Assets/Scripts/UI/PickCharacter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,74 +12,84 @@ public class PickCharacter : MonoBehaviour
     private int current1pCharacter = 1; //character number
     private int current2pCharacter = 1;
 
-    private float current1pPosx = -17f;
-    private float current2pPosx = -17f;
-    private Vector3 current1pPos = Vector3.zero;
-    private Vector3 current2pPos = Vector3.zero;
-    private Vector3 next1pPos = Vector3.zero;
-    private Vector3 next2pPos = Vector3.zero;
+    private Vector3 current1pPos;
+    private Vector3 current2pPos;
+    private Vector3 next1pPos;
+    private Vector3 next2pPos;
 
     private bool ready1p = false;
     private bool ready2p = false;
+
+    public IEnumerator Bar1pMove(Vector3 target)
+    {
+        while (Vector3.Distance(selectBar1p.transform.position, target) > 0.05f)
+        {
+            selectBar1p.transform.position = Vector3.Lerp(selectBar1p.transform.position, target, 10 * Time.deltaTime);
+            yield return null;
+        }
+        selectBar1p.transform.position = target;
+        yield break;
+    }
+
+    public IEnumerator Bar2pMove(Vector3 target)
+    {
+        while (Vector3.Distance(selectBar2p.transform.position, target) > 0.05f)
+        {
+            selectBar2p.transform.position = Vector3.Lerp(selectBar2p.transform.position, target, 10 * Time.deltaTime);
+            yield return null;
+        }
+        selectBar2p.transform.position = target;
+        yield break;
+    }
 
     private void Start()
     {
         current1pPos = selectBar1p.transform.position;
         current2pPos = selectBar2p.transform.position;
-        StartCoroutine(SmoothMove());
+        next1pPos = current1pPos;
+        next2pPos = current2pPos;
+        Debug.Log(current1pPos);
+        Debug.Log(current2pPos);
     }
 
 
-    IEnumerator SmoothMove()
+    private void Update()
     {
-        if (!ready1p)
+        if (!ready1p && Vector3.Distance(selectBar1p.transform.position, next1pPos) <= 0.05f)
         {
             if (current1pCharacter < 3 && Input.GetKeyDown(KeyCode.D))
             {
-
-                current1pPosx = current1pPos.x;
-                /*selectBar1p.transform.position = new Vector3(current1pPosx + 7, 0, 0);*/
-
-                next1pPos = new Vector3(current1pPosx + 7, 0, 0);
-                selectBar1p.transform.position = Vector3.Lerp(current1pPos, new Vector3(-17,0,-900), Time.deltaTime);
-                yield return new WaitForSeconds(1f);
-                current1pPos = next1pPos;
+                current1pPos = selectBar1p.transform.position;
+                next1pPos = current1pPos + new Vector3(7, 0, 0);
+                StartCoroutine(Bar1pMove(next1pPos));
                 current1pCharacter++;
+
             }
 
             if (current1pCharacter > 1 && Input.GetKeyDown(KeyCode.A))
             {
-                current1pPosx = current1pPos.x;
-                /*selectBar1p.transform.position = new Vector3(current1pPosx - 7, 0, 0);*/
-
-                next1pPos = new Vector3(current1pPosx - 7, 0, 0);
-                selectBar1p.transform.position = Vector3.Lerp(current1pPos, next1pPos, Time.deltaTime);
-                current1pPos = next1pPos;
+                current1pPos = selectBar1p.transform.position;
+                next1pPos = current1pPos + new Vector3(-7, 0, 0);
+                StartCoroutine(Bar1pMove(next1pPos));
                 current1pCharacter--;
             }
         }
 
-        if (!ready2p)
+        if (!ready2p && Vector3.Distance(selectBar2p.transform.position, next2pPos) <= 0.05f)
         {
             if (current2pCharacter < 3 && Input.GetKeyDown(KeyCode.RightArrow))
             {
-                current2pPosx = current2pPos.x;
-                /*selectBar2p.transform.position = new Vector3(current2pPosx + 7, 0, 0);*/
-
-                next2pPos = new Vector3(current2pPosx + 7, 0, 0);
-                selectBar2p.transform.position = Vector3.Lerp(current2pPos, next2pPos, 1f);
-                current2pPos = next2pPos;
+                current2pPos = selectBar2p.transform.position;
+                next2pPos = current2pPos + new Vector3(7, 0, 0);
+                StartCoroutine(Bar2pMove(next2pPos));
                 current2pCharacter++;
             }
 
             if (current2pCharacter > 1 && Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                current2pPosx = current2pPos.x;
-                /*selectBar2p.transform.position = new Vector3(current2pPosx - 7, 0, 0);*/
-
-                next2pPos = new Vector3(current2pPosx - 7, 0, 0);
-                selectBar2p.transform.position = Vector3.Lerp(current2pPos, next2pPos, 1f);
-                current2pPos = next2pPos;
+                current2pPos = selectBar2p.transform.position;
+                next2pPos = current2pPos + new Vector3(-7, 0, 0);
+                StartCoroutine(Bar2pMove(next2pPos));
                 current2pCharacter--;
             }
         }
@@ -95,6 +106,5 @@ public class PickCharacter : MonoBehaviour
         ready2p = true;
         Debug.Log("2P ready: " + current2pCharacter + " selected.");
     }
+
 }
-
-
